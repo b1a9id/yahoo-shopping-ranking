cffbda705ca388b8aea100876fa6175e85ea2f0a
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2018-06-10 15:46:14 +0900

    introduce Redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// Storeの生成
+const store = createStore(
+	// 1つのReducerで完結することはほぼないので、
+	// 最初からcombineReducersを使う実装にしておく
+	combineReducers(reducers),
+	// Redux Middlewareにredux-loggerを設定
+	applyMiddleware(logger)
+);
+
+ReactDOM.render(
+	// Store を Appコンポーネントに紐付け
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
