9334eb9e6c7cb60b7db4afe6ae095e14b52a6d60
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2018-06-10 17:03:13 +0900

    introduce page routing

--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
 import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import createBrowserHistory from 'history/createBrowserHistory';
 import App from './App';
-import * as reducers from './reducers';
+import createStore from './createStore';
+
+// historyのインスンタンスを生成
+const history = createBrowserHistory();
 
 // Storeの生成
-const store = createStore(
-	// 1つのReducerで完結することはほぼないので、
-	// 最初からcombineReducersを使う実装にしておく
-	combineReducers(reducers),
-	// Redux Middlewareにredux-loggerを設定
-	applyMiddleware(logger)
-);
+const store = createStore(history);
 
 ReactDOM.render(
 	// Store を Appコンポーネントに紐付け
 	<Provider store={store}>
-		<App />
+		{
+			/*
+			Link コンポーネントなどが動作するように
+			react-router-domのRouterではなく
+			react-router-reduxのConnectedRouterを使う
+			 */
+		}
+		<ConnectedRouter history={history}>
+			<App />
+		</ConnectedRouter>
 	</Provider>,
 	document.getElementById('root')
 );
