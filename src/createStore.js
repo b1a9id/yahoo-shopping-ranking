1cf538ce7164214fd8ebcce83a5afba72d07d04f
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2018-06-11 23:56:17 +0900

    implement async processing

--- src/createStore.js
+++ src/createStore.js
@@ -5,6 +5,7 @@ import {
 	applyMiddleware
 } from 'redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import { routerReducer, routerMiddleware } from 'react-router-redux';
 
 import * as reducers from './reducers';
@@ -19,6 +20,7 @@ export default function createStore(history) {
 		}),
 		applyMiddleware(
 			logger,
+			thunk,
 			// react-router-reduxのRedux Middleware
 			routerMiddleware(history)
 		)
