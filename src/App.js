9c1e61f9e05f519d1decc364374dba57d7b2c6c0
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2018-06-15 00:04:43 +0900

    add Title

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import React, {Component} from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom';
 import Ranking from './containers/Ranking';
 import Nav from './containers/Nav';
+import Reboot from 'material-ui/Reboot';
+import AppBar from 'material-ui/AppBar';
+import ToolBar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
+				<Reboot/>
+
+				<AppBar>
+					<ToolBar>
+						<Typography type="title" color="inherit">
+							Yahoo!ショッピングランキング
+						</Typography>
+					</ToolBar>
+				</AppBar>
+
 				<Nav />
 
 				<Switch>
